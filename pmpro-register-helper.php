f2cf43b9015135bb85bfa3f98661a21e97c54e47
Jason Coleman <jason@strangerstudios.com>
2014-04-14 12:49:38 -0400

    Fixed version

--- pmpro-register-helper.php
+++ pmpro-register-helper.php
@@ -10,7 +10,7 @@ Author URI: http://www.strangerstudios.com
 
 define('PMPRORH_DIR', dirname(__FILE__) );
 define('PMPRORH_URL', WP_PLUGIN_URL . "/pmpro-register-helper");
-define('PMPRORH_VERSION', '.5.12');
+define('PMPRORH_VERSION', '.5.13');
 
 /*
 	options - just defaults for now, will be in settings eventually

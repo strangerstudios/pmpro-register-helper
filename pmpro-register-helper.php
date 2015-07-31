8aac58bcbf8a8ca897ff293adcd9343cf3d56866
Jason Coleman <jason@strangerstudios.com>
2015-07-31 17:27:09 -0400

    Moved pmpro_signup to it's own file in /shortcodes/. Fixed recaptcha, and some warnings.

--- pmpro-register-helper.php
+++ pmpro-register-helper.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Paid Memberships Pro - Register Helper Add On
 Plugin URI: http://www.paidmembershipspro.com/pmpro-register-helper/
-Description: Shortcodes and other functions to help customize your registration forms.
+Description: Custom fields, shortcodes, and other functions to help customize your Paid Memberships Pro checkout process.
 Version: 1.0.1
 Author: Stranger Studios
 Author URI: http://www.strangerstudios.com
@@ -23,6 +23,11 @@ global $pmprorh_options;
 //$pmprorh_options["use_email_for_login"] = true;
 //$pmprorh_options["directory_page"] = "/directory/";
 //$pmprorh_options["profile_page"] = "/profile/";
+
+/*
+	Includes
+*/
+require_once(PMPRORH_DIR . "/shortcodes/pmpro_signup.php");			//[pmpro_signup ...] shortcode
 
 /*
 	Loading Modules
@@ -47,7 +52,7 @@ if(!empty($pmprorh_options) && !empty($pmprorh_options['modules']))
 			require_once($custom_file);	
 		}
 		else
-			require_once(dirname(__FILE__) . "/modules/".$value.".php");
+			require_once(PMPRORH_DIR . "/modules/".$value.".php");
 	}
 }
 	
@@ -916,134 +921,6 @@ function pmprorh_rf_save_extra_profile_fields( $user_id )
 }
 add_action( 'personal_options_update', 'pmprorh_rf_save_extra_profile_fields' );
 add_action( 'edit_user_profile_update', 'pmprorh_rf_save_extra_profile_fields' );
-
-/*
-	This shortcode will show a signup form. It will only show user account fields.
-	If the level is not free, the user will have to enter the billing information on the checkout page.	
-*/
-function pmprorh_signup_shortcode($atts, $content=null, $code="")
-{
-	// $atts    ::= array of attributes
-	// $content ::= text within enclosing form of shortcode element
-	// $code    ::= the shortcode found, when == callback name
-	// examples: [pmpro_signup level="3" short="1" intro="0" button="Signup Now"]
-
-	extract(shortcode_atts(array(
-		'button' => "Sign Up Now",
-		'intro' => "0",
-		'level' => NULL,		
-		'login' => true,
-		'short' => NULL,
-		'title' => "Register For " . pmpro_getLevel($level)->name,
-	), $atts));
-	
-	//turn 0's into falses
-	if($login === "0" || $login === "false" || $login === "no")
-		$login = false;
-	else
-		$login = true;
-
-	if($short === "0" || $short === "false" || $short === "no")
-		$short = false;
-	else
-		$short = true;
-	
-	if($intro === "0" || $intro === "false" || $intro === "no")
-		$intro = false;
-
-	global $current_user, $membership_levels;
-
-	ob_start();	
-	?>
-		<?php if(!empty($current_user->ID) && pmpro_hasMembershipLevel($level,$current_user->ID)) { ?>
-			<p>You are logged in as <?php echo $current_user->user_login; ?>.</p>
-		<?php } else { ?>
-		<form class="pmpro_form pmpro_signup_form" action="<?php echo pmpro_url("checkout"); ?>" method="post">
-			<h2><?php echo $title; ?></h2>
-			<?php
-				if(!empty($intro))
-					echo wpautop($intro);
-			?>
-			<input type="hidden" id="level" name="level" value="<?php echo $level; ?>" />	
-			<?php
-				if(!empty($current_user->ID))
-				{
-					?>
-					<p id="pmpro_account_loggedin">
-						<?php printf(__('You are logged in as <strong>%s</strong>. If you would like to use a different account for this membership, <a href="%s">log out now</a>.', 'pmpro'), $current_user->user_login, wp_logout_url($_SERVER['REQUEST_URI'])); ?>			
-					</p>
-					<?php
-				}
-				else
-				{
-					?>
-					<div>
-						<label for="username">Username</label>
-						<input id="username" name="username" type="text" class="input" size="30" value="" /> 
-					</div>
-					<?php do_action("pmpro_checkout_after_username");?>
-					<div>
-						<label for="password">Password</label>
-						<input id="password" name="password" type="password" class="input" size="30" value="" /> 
-					</div>
-					<?php if($short) { ?>
-						<input type="hidden" name="password2_copy" value="1" />
-					<?php } else { ?>
-						<div>
-							<label for="password2">Confirm Password</label>
-							<input id="password2" name="password2" type="password" class="input" size="30" value="" /> 
-						</div>			
-					<?php } ?>
-					<?php do_action("pmpro_checkout_after_password");?>
-					<div>
-						<label for="bemail">E-mail Address</label>
-						<input id="bemail" name="bemail" type="email" class="input" size="30" value="" /> 
-					</div>
-					<?php if($short) { ?>
-						<input type="hidden" name="bconfirmemail_copy" value="1" />
-					<?php } else { ?>
-						<div>
-							<label for="bconfirmemail">Confirm E-mail</label>
-							<input id="bconfirmemail" name="bconfirmemail" type="email" class="input" size="30" value="" /> 
-						</div>	         
-					<?php } ?>
-					<?php do_action("pmpro_checkout_after_email");?>
-					<div class="pmpro_hidden">
-						<label for="fullname">Full Name</label>
-						<input id="fullname" name="fullname" type="text" class="input" size="30" value="" /> <strong>LEAVE THIS BLANK</strong>
-					</div>
-					
-					<div class="pmpro_captcha">
-						<?php 																								
-							global $recaptcha, $recaptcha_publickey;										
-							if($recaptcha == 2 || ($recaptcha == 1 && pmpro_isLevelFree($pmpro_level))) 
-							{											
-								echo recaptcha_get_html($recaptcha_publickey, NULL, true);						
-							}								
-						?>								
-					</div>
-					<?php
-				}
-			?>			
-			<div>
-				<span id="pmpro_submit_span" >
-					<input type="hidden" name="submit-checkout" value="1" />		
-					<input type="submit" class="pmpro_btn pmpro_btn-submit-checkout" value="<?php echo $button; ?>" />
-				</span>
-			</div>	
-			<?php if(!empty($login) && empty($current_user->ID)) { ?>
-			<div style="text-align:center;">
-				<a href="<?php echo wp_login_url(get_permalink()); ?>"><?php _e('Log In','pmpro'); ?></a>
-			</div>	
-			<?php } ?>
-		</form>
-		<?php } ?>
-	<?php
-	$temp_content = ob_get_contents();
-	ob_end_clean();
-	return $temp_content;
-}
-add_shortcode("pmpro_signup", "pmprorh_signup_shortcode");
 
 /*
 	This code can be used to restrict level signups by email address or username
---
+++ shortcodes/pmpro_signup.php
@@ -0,0 +1,139 @@
+<?php
+/*
+	This shortcode will show a signup form. It will only show user account fields.
+	If the level is not free, the user will have to enter the billing information on the checkout page.
+*/
+function pmprorh_signup_shortcode($atts, $content=null, $code="")
+{
+	// $atts    ::= array of attributes
+	// $content ::= text within enclosing form of shortcode element
+	// $code    ::= the shortcode found, when == callback name
+	// examples: [pmpro_signup level="3" short="1" intro="0" button="Signup Now"]
+
+	//make sure PMPro is activated
+	if(!function_exists('pmpro_getLevel'))
+		return "Paid Memberships Pro must be installed to use the pmpro_signup shortcode.";
+
+	//default title
+	if(!empty($level))
+		$default_title = 'Register For ' . pmpro_getLevel($level)->name;
+	else
+		$default_title = 'Register For ' . get_option('blogname');
+
+	//set defaults
+	extract(shortcode_atts(array(
+		'button' => "Sign Up Now",
+		'intro' => "0",
+		'level' => NULL,
+		'login' => true,
+		'short' => NULL,
+		'title' => $default_title,
+	), $atts));
+
+	//turn 0's into falses
+	if($login === "0" || $login === "false" || $login === "no")
+		$login = false;
+	else
+		$login = true;
+
+	if($short === "0" || $short === "false" || $short === "no")
+		$short = false;
+	else
+		$short = true;
+
+	if($intro === "0" || $intro === "false" || $intro === "no")
+		$intro = false;
+
+	global $current_user, $membership_levels;	
+	
+	ob_start();
+	?>
+		<?php if(!empty($current_user->ID) && pmpro_hasMembershipLevel($level,$current_user->ID)) { ?>
+			<p>You are logged in as <?php echo $current_user->user_login; ?>.</p>
+		<?php } else { ?>
+		<form class="pmpro_form pmpro_signup_form" action="<?php echo pmpro_url("checkout"); ?>" method="post">
+			<h2><?php echo $title; ?></h2>
+			<?php
+				if(!empty($intro))
+					echo wpautop($intro);
+			?>
+			<input type="hidden" id="level" name="level" value="<?php echo $level; ?>" />
+			<?php
+				if(!empty($current_user->ID))
+				{
+					?>
+					<p id="pmpro_account_loggedin">
+						<?php printf(__('You are logged in as <strong>%s</strong>. If you would like to use a different account for this membership, <a href="%s">log out now</a>.', 'pmpro'), $current_user->user_login, wp_logout_url($_SERVER['REQUEST_URI'])); ?>
+					</p>
+					<?php
+				}
+				else
+				{
+					?>
+					<div>
+						<label for="username">Username</label>
+						<input id="username" name="username" type="text" class="input" size="30" value="" />
+					</div>
+					<?php do_action("pmpro_checkout_after_username");?>
+					<div>
+						<label for="password">Password</label>
+						<input id="password" name="password" type="password" class="input" size="30" value="" />
+					</div>
+					<?php if($short) { ?>
+						<input type="hidden" name="password2_copy" value="1" />
+					<?php } else { ?>
+						<div>
+							<label for="password2">Confirm Password</label>
+							<input id="password2" name="password2" type="password" class="input" size="30" value="" />
+						</div>
+					<?php } ?>
+					<?php do_action("pmpro_checkout_after_password");?>
+					<div>
+						<label for="bemail">E-mail Address</label>
+						<input id="bemail" name="bemail" type="email" class="input" size="30" value="" />
+					</div>
+					<?php if($short) { ?>
+						<input type="hidden" name="bconfirmemail_copy" value="1" />
+					<?php } else { ?>
+						<div>
+							<label for="bconfirmemail">Confirm E-mail</label>
+							<input id="bconfirmemail" name="bconfirmemail" type="email" class="input" size="30" value="" />
+						</div>
+					<?php } ?>
+					<?php do_action("pmpro_checkout_after_email");?>
+					<div class="pmpro_hidden">
+						<label for="fullname">Full Name</label>
+						<input id="fullname" name="fullname" type="text" class="input" size="30" value="" /> <strong>LEAVE THIS BLANK</strong>
+					</div>
+
+					<div class="pmpro_captcha">
+						<?php
+							global $recaptcha, $recaptcha_publickey;							
+							if($recaptcha == 2 || (!empty($level) && $recaptcha == 1 && pmpro_isLevelFree(pmpro_getLevel($level))))
+							{
+								echo pmpro_recaptcha_get_html($recaptcha_publickey, NULL, true);
+							}
+						?>
+					</div>
+					<?php
+				}
+			?>
+			<div>
+				<span id="pmpro_submit_span" >
+					<input type="hidden" name="submit-checkout" value="1" />
+					<input type="submit" class="pmpro_btn pmpro_btn-submit-checkout" value="<?php echo $button; ?>" />
+				</span>
+			</div>
+			<?php if(!empty($login) && empty($current_user->ID)) { ?>
+			<div style="text-align:center;">
+				<a href="<?php echo wp_login_url(get_permalink()); ?>"><?php _e('Log In','pmpro'); ?></a>
+			</div>
+			<?php } ?>
+		</form>
+		<?php } ?>
+	<?php
+	$temp_content = ob_get_contents();
+	ob_end_clean();
+	return $temp_content;
+}
+add_shortcode("pmpro_signup", "pmprorh_signup_shortcode");

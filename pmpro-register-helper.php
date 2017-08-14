2578a4c6a1a7f35bade2ccfa2a047db4c5948d2a
Ted Barnett <ted@contemplatedesign.com>
2017-08-14 09:42:45 -0400

    need to check if $_Files[]['name'] value is empty

--- pmpro-register-helper.php
+++ pmpro-register-helper.php
@@ -618,7 +618,7 @@ function pmprorh_rf_pmpro_registration_checks($okay)
 				else
 					$value = false;
 			 
-				if(!empty($field->required) && empty( $_REQUEST[$field->name] ) && empty( $_FILES[$field->name] ) )
+				if(!empty($field->required) && empty( $_REQUEST[$field->name] ) && empty( $_FILES[$field->name]['name'] ) )
 				{
 					$required[] = $field->name;
                     $required_labels[] = $field->label;

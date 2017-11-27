fdfe2933e7de3cda76f7422ed5b4cdc07ab14def
jbruggeling <jbruggeling@users.noreply.github.com>
2017-11-27 14:00:08 +1030

    span tag issue
    Removed redundant span tag causing HTML5 Validation Issues

--- classes/class.field.php
+++ classes/class.field.php
@@ -600,7 +600,7 @@
 				if(is_string($this->showrequired))
 					$r .= $this->showrequired;
 				else
-					$r .= '<span class="pmpro_asterisk"> <abbr title="Required Field">*</abbr></span></span>';
+					$r .= '<span class="pmpro_asterisk"> <abbr title="Required Field">*</abbr></span>';
 			}
 
 			//anything meant to be added to the beginning or end?
